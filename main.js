0e836cd77d7b526523cf4fcb682e08459ef6d4d1
Ethan Jurman <ethanjurman@gmail.com>
2025-02-01 03:21:51 -0500

    script

--- main.js
+++ main.js
@@ -4,14 +4,15 @@ const ctx = canvas.getContext("2d");
 let frame = 0;
 let lines = [];
 let gameState = {
-    state: "START",
+    state: "READY",
     creature: {
         x: -1,
         y: -1,
     },
-    radarKey: -1,
+    radarKey: "",
     queuedActions: [],
     unqueuedActions: [],
+    cargo: [],
 };
 function addQueuedAction(action) {
     gameState.queuedActions.push(action);
@@ -19,38 +20,56 @@ function addQueuedAction(action) {
 function addUnqueuedAction(action) {
     gameState.unqueuedActions.push(action);
 }
+function advanceQueuedAction() {
+    gameState.queuedActions[0] = Object.assign(Object.assign({}, gameState.queuedActions[0]), { delay: -1 });
+}
+function addTextAction(...texts) {
+    texts.forEach((text, index) => {
+        const prevText = texts[index - 1] || "";
+        addQueuedAction({
+            type: "text",
+            delay: Math.max(prevText.split(" ").length * 50, 180),
+            data: { text },
+        });
+    });
+}
+const executeAction = (action) => {
+    if (action.delay > 0) {
+        return true;
+    }
+    if (action.type === "text") {
+        const linesContainer = document.getElementById("lines");
+        const newLine = document.createElement("div");
+        newLine.innerText = action.data.text;
+        linesContainer.appendChild(newLine);
+        if (linesContainer.childElementCount > 4) {
+            const targetChild = linesContainer.children[linesContainer.childElementCount - 5];
+            targetChild.setAttribute("style", "display: none");
+        }
+    }
+    return false;
+};
 function gameLoop() {
     // execute queued actions
-    gameState.queuedActions = gameState.queuedActions.map((action) => {
+    gameState.queuedActions = gameState.queuedActions.map((action, index) => {
+        if (index !== 0) {
+            return action;
+        }
         return Object.assign(Object.assign({}, action), { delay: action.delay - 1 });
     });
-    gameState.queuedActions = gameState.queuedActions.filter((action) => {
-        if (action.delay > 0) {
-            return true;
-        }
-        if (action.type === "text") {
-            const linesContainer = document.getElementById("lines");
-            const newLine = document.createElement("div");
-            newLine.innerText = action.data.text;
-            linesContainer.appendChild(newLine);
-            if (linesContainer.childElementCount > 4) {
-                const targetChild = linesContainer.children[linesContainer.childElementCount - 5];
-                targetChild.setAttribute("style", "display: none");
-            }
-        }
-        return false;
+    gameState.unqueuedActions = gameState.unqueuedActions.map((action) => {
+        return Object.assign(Object.assign({}, action), { delay: action.delay - 1 });
     });
+    gameState.queuedActions = gameState.queuedActions.filter(executeAction);
+    gameState.unqueuedActions = gameState.unqueuedActions.filter(executeAction);
 }
 document.onkeydown = function (e) {
     // key code for space
+    if (e.code === "Enter") {
+        addTextAction("BOOTING UP CARGO BAY COMPUTER INTELEGENCE...", "GOOD MORNING CAPTAIN.", "WE HAVE WOKEN YOU EARLY FROM CRYO SLEEP TO DEAL WITH AN ANOMOLY IN THE CARGO BAY.", "WE BELIEVE A CREATURE FROM CRATE X72 WAS NOT PROPERLY CONTAINED IS NOW DESTROYING NEIGHBORING CARGO UNITS.", "THIS IS AN UNACCEPTABLE LOSS FOR THE CORPERATION.", "THERE ARE 36 CONTAINERS IN THE CARGO BAY. THEY ARE LABELED AND LOCATED BASED ON THE KEYBOARD CONFIGURATION IN FRONT OF YOU.", "1 THROUGH 9 TO Z THROUGH PERIOD ARE ALL VALID CARGO KEYS.", "PRESS ANY CARGO KEY ON YOUR KEYBOARD TO ACTIVATE ITS DOPPLER-RADAR FROM ITS RESPECTIVE LOCATION.", "PRESS SHIFT PLUS ANY CARGO KEY TO EJECT THE CONTAINER INTO SPACE.", "IDENTIFY THE CREATURES LOCATION.", "EJECT IT.", "DO NOT EJECT AN EXTRANIOUS CARGO, YOU WILL BE FINED FOR ANY UNECESSARY LOSSES.", "PRESS ENTER TO BEGIN.");
+    }
     if (e.code === "Space") {
-        addQueuedAction({
-            type: "text",
-            delay: 0,
-            data: {
-                text: "Hello world " + Math.floor(Math.random() * 100),
-            },
-        });
+        advanceQueuedAction();
     }
 };
 function animate() {
